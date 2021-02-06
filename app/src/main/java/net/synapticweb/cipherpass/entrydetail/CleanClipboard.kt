763a6adda888fb148b958e9d36c8b4c26d7f8ca4
synapticweb <synapticwebb@gmail.com>
2021-02-06 22:36:27 +0200

    Added copyright notice.

--- app/src/main/java/net/synapticweb/cipherpass/entrydetail/CleanClipboard.kt
+++ app/src/main/java/net/synapticweb/cipherpass/entrydetail/CleanClipboard.kt
@@ -1,3 +1,9 @@
+/*
+ * Copyright (C) 2021 Eugen Rădulescu <synapticwebb@gmail.com>
+ * This file is part of the CipherPass project ( https://github.com/synapticweb/CipherPass ). 
+ * See the LICENSE file in the project root for license terms. 
+ */
+
 package net.synapticweb.cipherpass.entrydetail
 
 import android.content.ClipData
